28759cc523c2a8cfb839ce39184a50d4fb0ef0b9
Amir Mor <amirmor75@gmail.com>
2021-11-06 13:09:44 +0200

    all functions placed

--- src/main/java/worker.java
+++ src/main/java/worker.java
@@ -1,22 +1,27 @@
+import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+import javax.xml.parsers.ParserConfigurationException;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+
+import org.apache.pdfbox.tools.PDFText2HTML;
+
+
 
 
 public class worker {
     public static void main(String[] args) {
 
-        convertPdfToImage("amir.pdf","C:\\Users\\Amir\\Desktop\\distributed operating systems 1\\amir.pdf");
+        //generateHTMLFromPDF("C:\\Users\\Amir\\Desktop\\distributed operating systems 1\\amir.pdf")
     }
 
-/*
-    public static int convertPdf(String url,String action){
+    public static String downloadPdf(String url) {
         String[] arrSplit=url.split("/",1);
         String name = arrSplit[arrSplit.length-1];
         int CONNECT_TIMEOUT= 1000;
@@ -29,47 +34,58 @@ public class worker {
                     READ_TIMEOUT);
         } catch () {
         }
+        return null; //source path name
+    }
+
+    public static void convertPdf(String fileName,String sourcePath,String action){
+
         // load pdf to an object in order to convert it
-        File file = new File(pathname);
+        File file = new File(sourcePath);
         PDDocument document = null;
         try {
             document = Loader.loadPDF(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //PDDocument document = PDDocument.load(new File(pdfFilename));
-        PDFRenderer pdfRenderer = new PDFRenderer(document);
-
-
-
-
         //switch case
             switch (action) {
                 case "ToImage":
+                    convertPdfToImage(fileName,"","",document);
                     break;
                 case "ToHTML":
+                    //convertPdfToHtml(fileName,"","",document);
                     break;
                 case "ToText":
-                    convertPdfToText(file);
+                    convertPdfToText(fileName,"","",document);
+                    break;
             }
 
     }
-        return 0;
-    }
-    */
 
 
-    public static int convertPdfToText(PDDocument document){
+
+
+
+    public static int convertPdfToText(String fileName,String sourcePath ,String destinationPath,PDDocument document){
+
         try{
 
             PDFTextStripper pdfStripper = new PDFTextStripper();
 
             //Retrieving text from PDF document
             String text = pdfStripper.getText(document);
-            System.out.println(text);
-
             //Closing the document
             document.close();
+
+            File textFile = new File(fileName + ".txt");
+            FileWriter myWriter = new FileWriter(textFile);
+            myWriter.write(text);
+            myWriter.close();
+
+            /*PrintWriter out = new PrintWriter(pdfFilename + ".txt");
+            out.println(text);
+            out.close();*/
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -77,20 +93,17 @@ public class worker {
     }
 
 
-//    private void convertPdfToHtml(PDDocument pdf) {
-//        Writer output = new PrintWriter("src/output/pdf.html", "utf-8");
-//        new PDFDomTree().writeText(pdf, output);
-//        output.close();
-//    }
-
-    public static void convertPdfToImage(String pdfFilename, String pathname){
-        File file = new File(pathname);
-        PDDocument document = null;
-        try {
-            document = Loader.loadPDF(file);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void convertPdfToHtml(String fileName,String sourcePath ,String destinationPath,PDDocument document) throws ParserConfigurationException, IOException {
+        PDFText2HTML parser = new PDFText2HTML();
+        Writer output = new PrintWriter(destinationPath+"\\"+fileName, "utf-8");
+        parser.writeText(document, output);
+        output.close();
+        if (document != null) {
+            document.close();
         }
+    }
+
+    public static void convertPdfToImage(String fileName,String sourcePath ,String destinationPath,PDDocument document){
         PDFRenderer pdfRenderer = new PDFRenderer(document);
         int page = 0;
         BufferedImage bim = null;
@@ -101,7 +114,7 @@ public class worker {
         }
 
         try {
-            ImageIOUtil.writeImage(bim, pdfFilename + "-" + (page+1) + ".png", 300);
+            ImageIOUtil.writeImage(bim, fileName + "-" + (page+1) + ".png", 300);
         } catch (IOException e) {
             e.printStackTrace();
         }
